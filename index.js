8381ae85846945bfa679c941766b23a4c1ce8ab3
Anthony Johnson <aj72325@gmail.com>
2022-02-22 13:53:12 -0700

    Filename Selection
    Per #3, the user can select a filename now as opposed to having to
    try and type it out.

--- index.js
+++ index.js
@@ -7,14 +7,68 @@ let prompt = require('prompt-sync')({sigint: true});
 let packageJSON = require('./package.json');
 
 let xd = new XDFile();
+let resourceFiles = [];
+let filename = '';
+
+// Load options for extraction
+let tmpFiles = fs.readdirSync('./resources');
+
+tmpFiles.forEach(function(file){
+
+  if(file.endsWith('.xd')) {
+
+    resourceFiles.push(file);
+
+  }
+
+});
 
 // Welcome and user input for filename
 console.log('Welcome to the TM Basic Theming Automater!');
 console.log(`v${packageJSON.version}`);
 console.log('\n');
-console.log('Please enter the name of your XD file (without extension).');
-console.log('IMPORTANT: Make sure your file is in the resources folder!');
-let filename = prompt();
+while(true) {
+
+  console.log('What file would you like to export?');
+  resourceFiles.forEach(function(file, index){console.log(`${index + 1}) ${file}`);});
+  let option = prompt();
+  let valid = true;
+
+  if(isNaN(option)) {
+
+    valid = false;
+    console.log('You entered an invalid number. Please enter only the number of the option ' + 
+      'that corresponds with your choice. Press enter to try again.');
+    prompt();
+
+  }
+  else {
+
+    option = parseInt(option);
+    if(option > resourceFiles.length || option <= 0) {
+
+      valid = false;
+      console.log('You entered a number that is not an option. Press enter to try again.');
+      prompt();
+
+    }
+    else {
+
+      filename = resourceFiles[option - 1];
+      // 3 is used because that is the length of .xd ending, we want the filename without extension
+      filename = filename.substring(0, filename.length - 3);
+
+    }
+
+  }
+
+  if(valid) {
+
+    break;
+
+  }
+
+}
 
 // Extract CSS from XD using instance of IExtractCSSFromXD
 console.log('Working, please wait.');
