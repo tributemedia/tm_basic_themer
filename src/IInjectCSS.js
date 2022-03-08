53afc1dbb56c9b4736e73e86a08d9da1e1a73618
Anthony Johnson <aj72325@gmail.com>
2022-03-08 13:12:23 -0700

    IInjectCSS Interface
    Created an interface for injecting CSS into destinations. This
    work was apart of #9.

---
+++ src/IInjectCSS.js
@@ -0,0 +1,11 @@
+
+/**
+ * Javascript interface for defining injectors of CSS into a data destination.
+ */
+let IInjectCSS = class {
+  inject(css) {
+    console.log('The extender of this class has not implemented an override of this method.');
+  }
+}
+
+module.exports = IInjectCSS;
